ad7e253e9354fad53310f2240d49af135d91db11
Noah Franz <noahfranz13@gmail.com>
2024-11-25 09:26:42 -0700

    update logging

--- runbot.py
+++ runbot.py
@@ -1,24 +1,32 @@
 """
 CLI for running the slack bot through python
 """
+import os
 from tns_slack_bot import TNSSlackBot
 from astropy import units as u
 import argparse
 import logging
 
 logger = logging.getLogger(__name__)
-
 def main():
-    logging.basicConfig(filename="tns-slack-bot.log")
     
     p = argparse.ArgumentParser()
     p.add_argument("--delta_t", "-dt", required=False, help="The delta time in days to filter by", default=1)
     p.add_argument("--outfile", "-o", required=False, help="The filepath to write the TNS dataset to", default="tns_public_objects.csv")
     args = p.parse_args()
 
+    logging.basicConfig(
+        format='%(asctime)s %(message)s',
+        datefmt='%m/%d/%Y %I:%M:%S %p',
+        filename=os.path.join(os.path.dirname(args.outfile), 'tns-slack-bot.log'),
+        encoding='utf-8',
+        level=logging.DEBUG
+    )
+
+
     logger.info("Initializing Slack Bot...")
     try:
-        bot = TNSSlackBot(dt=float(args.delta_t)*u.day, daily_data_path=args.outfile)
+        bot = TNSSlackBot(dt=float(args.delta_t), daily_data_path=args.outfile)
     except Exception as e:
         logger.exception(e)
 
